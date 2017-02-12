1c57bd019e936aecc0cbb1fa30995fb7ce91dfc7
unknownartist-pau <orerhusam@gmail.com>
2017-02-12 18:03:47 +0300

    Add files via upload

---
+++ js/SpeakGoodBye.js
@@ -0,0 +1,18 @@
+﻿
+(function (window) {
+    var byeSpeaker = {};
+    var speakWordBye = {};
+
+    var speakWord = "Good Bye";
+    speakWordBye.bye = speakWord;
+
+
+    byeSpeaker.speak = function (name) {
+
+        console.log(speakWordBye.bye + " " + name);
+
+    }
+
+    window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ js/SpeakHello.js
@@ -0,0 +1,18 @@
+﻿(function (window) {
+
+
+
+    var helloSpeaker = {};
+    var speakWordHello = {};
+
+
+    var speakWord = "Hello";
+    speakWordHello.hello = speakWord;
+
+    helloSpeaker.speak = function (name) {
+
+        console.log(speakWordHello.hello + " " + name);
+    }
+
+    window.helloSpeaker = helloSpeaker;
+})(window);
